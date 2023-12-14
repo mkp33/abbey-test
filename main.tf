2a103348c1d40a6df6eff68a77d5bb0a5feaf1cc
mkp33 <minkyu.park.personal@gmail.com>
2023-12-14 12:18:43 +0900

    deploy starter kit

--- main.tf
+++ main.tf
@@ -30,20 +30,20 @@ resource "abbey_grant_kit" "abbey_demo_site" {
     steps = [
       {
         reviewers = {
-          one_of = ["replace-me@example.com"] # CHANGEME
+          one_of = ["minkyu.park.personal@gmail.com"] # CHANGEME
         }
       }
     ]
   }
 
   policies = [
-    { bundle = "github://replace-me-with-organization/replace-me-with-repo/policies" } # CHANGEME
+    { bundle = "github://mkp33/test-repository/policies" } # CHANGEME
   ]
 
   output = {
     # Replace with your own path pointing to where you want your access changes to manifest.
     # Path is an RFC 3986 URI, such as `github://{organization}/{repo}/path/to/file.tf`.
-    location = "github://replace-me-with-organization/replace-me-with-repo/access.tf" # CHANGEME
+    location = "github://mkp33/test-repository/access.tf" # CHANGEME
     append = <<-EOT
       resource "abbey_demo" "grant_read_write_access" {
         permission = "read_write"
